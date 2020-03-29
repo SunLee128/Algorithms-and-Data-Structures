061b185e9abaca9066bc9d80abbc22ebc99649c9
sunlee <sunlee128@gmail.com>
2020-03-30 09:08:14 +1100

    sliding window

--- ProblemSolvingPatterns/multiplePointers.js
+++ ProblemSolvingPatterns/multiplePointers.js
@@ -43,13 +43,69 @@ function sumZero (arr) {
   }
 }
 
-sumZero([
-  -4,
-  -3,
-  -2,
-  -1,
-  0,
-  1,
-  2,
-  5
-]);
+// sumZero([
+//   -4,
+//   -3,
+//   -2,
+//   -1,
+//   0,
+//   1,
+//   2,
+//   5
+// ]);
+
+//returns the first combination of two numbers that makes target
+var twoSum = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+    if (sum == target) {
+      return [
+        nums[left],
+        nums[right]
+      ];
+    } else if (sum > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
+
+console.log(
+  twoSum(
+    [
+      2,
+      7,
+      11,
+      15
+    ],
+    9
+  )
+); // [2,7]
+
+//returns number of unique values
+function countUniqueValues (arr) {
+  if (arr.length === 0) return 0;
+  var i = 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+console.log(
+  countUniqueValues([
+    1,
+    2,
+    2,
+    5,
+    7,
+    7,
+    99
+  ])
+); //5
+
