7de584c4ff896aff252331a21d8741116d3a9d0b
sunlee <sunlee128@gmail.com>
2020-04-05 11:04:20 +1000

    frequency counter

--- ProblemSolvingPatterns/frequencyCounter.js
+++ ProblemSolvingPatterns/frequencyCounter.js
@@ -11,20 +11,20 @@ same([1,2,1], [4,4,1]) // false (must be same frequency)
 */
 
 // Naive Solution, Time Complexity - O(N^2)
-function same (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2);
-    if (correctIndex === -1) {
-      return false;
-    }
-    console.log(arr2);
-    arr2.splice(correctIndex, 1);
-  }
-  return true;
-}
+// function same (arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     let correctIndex = arr2.indexOf(arr1[i] ** 2);
+//     if (correctIndex === -1) {
+//       return false;
+//     }
+//     console.log(arr2);
+//     arr2.splice(correctIndex, 1);
+//   }
+//   return true;
+// }
 
 // same(
 //   [
@@ -43,17 +43,21 @@ function same (arr1, arr2) {
 
 //Refactored, Time Complexity - O(n)
 function same (arr1, arr2) {
+  //check the length
   if (arr1.length !== arr2.length) {
     return false;
   }
   let frequencyCounter1 = {};
   let frequencyCounter2 = {};
+  //creating an object to count the same number of items for arr1
   for (let val of arr1) {
     frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
   }
+  //creating an object to count the same number of items for arr2
   for (let val of arr2) {
     frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
   }
+  //matching key and the number of occurrence in arr1 and arr2.
   for (let key in frequencyCounter1) {
     if (!(key ** 2 in frequencyCounter2)) {
       return false;
@@ -105,3 +109,27 @@ function validAnagram (first, second) {
 
 // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
 validAnagram('anagrams', 'nagaramm');
+
+/* sameFrequency
+Given two positive integers, find out if the two numbers have the same frequency of digits. */
+
+function sameFrequency (num1, num2) {
+  const strNum1 = num1.toString();
+  const strNum2 = num2.toString();
+  if (strNum1.length !== strNum2.length) return false;
+  let countNum1 = {};
+  let countNum2 = {};
+  for (let i = 0; i < strNum1.length; i++) {
+    countNum1[strNum1[i]] = (countNum1[strNum1] || 0) + 1;
+  }
+  for (let j = 0; j < strNum2.length; j++) {
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1;
+  }
+  for (let key in countNum1) {
+    if (countNum1[key] !== countNum2[key]) return false;
+  }
+  return true;
+}
+
+console.log(sameFrequency(182, 281));
+console.log(sameFrequency(34, 14));
