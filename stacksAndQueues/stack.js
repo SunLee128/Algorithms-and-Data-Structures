d3fd77ae0ac4b3f5a565c640c1095a41b6954c54
sunlee <sunlee128@gmail.com>
2020-04-12 16:57:41 +1000

    stack

---
+++ stacksAndQueues/stack.js
@@ -0,0 +1,44 @@
+//LIFO last in first out
+
+//Undo/redo
+//Managing function invocations
+//routing (the history onject)
+
+class Node {
+  constructor (value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor () {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  //O(1)
+  push (val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  //O(1)
+  pop () {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
