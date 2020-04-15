a0adc54707bff9f3945b6a74d68958076d463095
sunlee <sunlee128@gmail.com>
2020-04-15 15:39:00 +1000

    tree traversal

--- binarySearchTrees/bst.js
+++ binarySearchTrees/bst.js
@@ -69,6 +69,50 @@ class BinarySearchTree {
     }
     return false;
   }
+  BFS () {
+    var node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder () {
+    var data = [];
+    function traverse (node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSPostOrder () {
+    var data = [];
+    function traverse (node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSInOrder () {
+    var data = [];
+    function traverse (node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 //      10
@@ -83,3 +127,6 @@ tree.insert(11);
 tree.insert(2);
 tree.insert(16);
 tree.insert(7);
+tree.DFSPreOrder();
+tree.DFSPostOrder();
+tree.DFSInOrder();
